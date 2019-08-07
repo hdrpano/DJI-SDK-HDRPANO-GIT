0d465f026e08839d8b260d2e755ae97b1796aa69
hdrpano <kilian.eisenegger@bluewin.ch>
2019-08-07 08:48:06 +0200

    Initial Commit

---
+++ DJI-SDK-HDRPANO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DJI-SDK-HDRPANO
+//
+//  Created by Kilian Eisenegger on 07.08.19.
+//  Copyright © 2019 Kilian Eisenegger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
